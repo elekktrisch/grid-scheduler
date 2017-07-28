a75f9c5432ee00c03170809d958201d9737f296d
Roman Schlegel <romanschlegel@gmail.com>
2017-07-28 17:25:22 +0200

    added draggable events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,45 +12,38 @@ export class AppComponent {
 
   rows: Array<Array<Hour>> = [];
   headers: Array<Hour> = [];
-  events: Array<Event> = [
-    {
-      start: moment("2017-08-01", "YYYY-MM-DD"),
-      durationHours: 3,
-      resource: {
-        name: "Maule"
-      },
-      reservationHolder: {
-        name: "Peter"
-      }
-    }
-  ];
+  events: Array<Event> = [];
 
   rowHeight: number = 30;
   cellWidth: number = 10;
   hoursPerDay: number = 24;
+  drawingEvent: Event;
+  now: Moment;
+  calendarStart: Moment;
 
   ngOnInit(): void {
+    this.now = moment(moment().format("YYYY-MM-DD"), "YYYY-MM-DD");
+    this.calendarStart = this.now.clone();
     let headerIdx = 0;
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 20; i++) {
       this.rows[i] = [];
       for (let j = 0; j < 365 * 24; j++) {
-        let now: Moment = moment();
         if (i === 0) {
           if (j % 24 === 0) {
             this.headers[headerIdx++] = {
-              start: now.clone(),
-              formatted: now.format("DD.MM.YYYY")
+              start: this.now.clone(),
+              formatted: this.now.clone().format("DD.MM.YYYY")
             };
           }
-          now = now.add(1, "hours");
+          this.now = this.now.add(1, "hours");
         }
         var hour = {
-          start: now.clone(),
+          start: this.now.clone(),
           events: []
         };
         this.events.forEach(event => {
           if (hour.start.isAfter(event.start)
-            && hour.start.isBefore(event.start.add(event.durationHours, "hours"))) {
+            && hour.start.isBefore(event.start.clone().add(event.durationHours, "hours"))) {
             hour.events.push(event);
           }
         });
@@ -58,12 +51,71 @@ export class AppComponent {
       }
     }
   }
+
+  dragStart(event: any): void {
+    event.preventDefault();
+
+    if (this.drawingEvent) {
+      this.clickedNewEvent();
+    } else {
+      const coordinates = { x: event.offsetX, y: event.offsetY };
+      var start = this.headers[0].start.clone().add(coordinates.x / this.cellWidth, "hours");
+      var resourceIndex = Math.round(coordinates.y / this.rowHeight) - 1;
+      this.drawingEvent = {
+        start: start,
+        startCell: moment.duration(start.diff(this.calendarStart)).asHours(),
+        durationHours: 1,
+        resourceIndex: resourceIndex
+      };
+    }
+  }
+
+  drawing(event: any) {
+    event.preventDefault();
+    if (this.drawingEvent) {
+      this.updateDrawingEvent(this.drawingEvent, event);
+    }
+  }
+
+  dragEnd(event: any): void {
+    if (this.drawingEvent) {
+      if (event) {
+        const e = this.drawingEvent;
+        delete this.drawingEvent;
+        this.updateDrawingEvent(e, event);
+        this.events.push(e);
+      } else {
+        this.clickedNewEvent();
+      }
+    }
+  }
+
+  private updateDrawingEvent(e: Event, event: any) {
+    const coordinates = { x: event.offsetX, y: event.offsetY };
+    var end = this.headers[0].start.clone().add(coordinates.x / this.cellWidth, "hours");
+    var durationHours = moment.duration(end.diff(e.start)).asHours();
+    if (durationHours > 0) {
+      e.durationHours = durationHours;
+    }
+  }
+
+  eventClicked(event: Event): void {
+    console.log("event", event);
+  }
+
+  clickedNewEvent(): void {
+    if (this.drawingEvent) {
+      this.events.push(this.drawingEvent);
+      delete this.drawingEvent;
+    }
+  }
 }
 
-class Event {
+interface Event {
   start: Moment;
+  startCell: number;
   durationHours: number;
-  resource?: Resource;
+  resourceIndex: number;
   reservationHolder?: ReservationHolder;
 }
 
